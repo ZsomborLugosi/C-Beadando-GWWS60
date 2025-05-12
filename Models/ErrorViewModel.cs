564968e121a8be00a384af7da0651885869eea32
ZsomborLugosi <lugosi.zsombor@gmail.com>
2025-05-12 12:30:17 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GWWS60Beadando.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# C-Beadando-GWWS60
